519fc36db672547c39cfe1bf092327e2e0fe78d8
obba P <paschalobba@yahoo.com>
2018-06-29 03:25:57 +0100

    Feature: The currency converter API

---
+++ main.js
@@ -0,0 +1,46 @@
+const app = angular.module("myApp",[]);
+
+app.controller('myCtrl',($scope,$http)=>{
+
+    $scope.submit = ()=>{
+
+        let query = $scope.prevCurr + '_' + $scope.newCurr ;
+
+        $http({
+            method : "GET",
+            url : "https://free.currencyconverterapi.com/api/v5/convert?q=" + query + "&compact=ultra"
+        }).then(response=>{
+            console.log(response);
+            try{
+                let JSONobj = response.data;
+                let val = JSONobj[query];
+    
+                if(val){
+                    let total = val * $scope.currentValue;
+                    $scope.answer = Math.round(total * 100 / 100);
+                }
+            } catch(e) {
+                console.log('error',e);
+            }
+
+        }).catch(error=>{
+            console.log('error',error);
+        });
+    }
+
+    $scope.getCurrencies = ()=>{
+        console.log('hian');
+        $http({
+            method : "GET",
+            url : "https://free.currencyconverterapi.com/api/v5/currencies"
+        }).then(response=>{
+            
+            $scope.currencies = response.data.results;
+            console.log('currencies',$scope.currencies);
+        }).catch(error=>{
+            console.log('error',error);
+        });
+    }
+
+    
+})
